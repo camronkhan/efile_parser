fad1b84f7b6d5a384553b0f10256fd6d27567432
Camron Khan <camron.khan@zurichna.com>
2017-12-18 11:57:34 -0600

    updated dir structure

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import app from './app';
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (err) => {
+    if (err) return console.log(err);
+    return console.log(`Server listening on port ${this.port}`);
+});
